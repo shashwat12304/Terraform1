965f0b2d7729d99cbbd7212762128fd56874b92a
Avi Bhardwaj <97936385+abhardwaj21@users.noreply.github.com>
2022-09-28 23:38:07 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
+variable "Repository_Name" {
+  type    = string
+}
 resource "github_repository" "github-repo-1" {
-  name        = "terraform-demo-1"
+  name        = var.Repository_Name
   description = "My awesome codebase"
 
   visibility = "public"
