02465e5684dea5ca0c57a8ab5e772ab6721ac36f
Shashwat Sharma <shashwatsharma12304@gmail.com>
2022-10-01 02:32:21 +0530

    new_repository

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ variable "Repository_Name" {
   type    = string
   default = "testrepo"
 }
-resource github_repository github-repo-1 {
+resource "github_repository" "github-repo-1" {
   name        = var.Repository_Name
   description = "My Tf codebase"
   visibility =  "public"
